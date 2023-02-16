3a67d91cfdf3c5347bdc34f120d8cb4b785a2d32
SeYeol00 <dmot0126@gmail.com>
2023-02-16 18:26:23 +0900

    commit

--- src/main/java/com/cos/jwtex01/config/CorsConfig.java
+++ src/main/java/com/cos/jwtex01/config/CorsConfig.java
@@ -13,11 +13,12 @@ public class CorsConfig {
    public CorsFilter corsFilter() {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
-      config.setAllowCredentials(true);
-      config.addAllowedOrigin("*"); // e.g. http://domain1.com
-      config.addAllowedHeader("*");
-      config.addAllowedMethod("*");
+      config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지를 결정
+      config.addAllowedOrigin("*"); // e.g. http://domain1.com 모든 ip에 응답 허용
+      config.addAllowedHeader("*"); // 모든 헤더에 응답을 허용
+      config.addAllowedMethod("*");// 모든 get, post put, delete, patch 요청을 허용하겠다.
 
+      // 등록, 해당 url에서 들어오는 모든 요청은 이걸 따른다.
       source.registerCorsConfiguration("/api/**", config);
       return new CorsFilter(source);
    }
--- src/main/java/com/cos/jwtex01/config/SecurityConfig.java
+++ src/main/java/com/cos/jwtex01/config/SecurityConfig.java
@@ -29,22 +29,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+				// 모든 요청은 이 필터에서 걸린다. @CrossOrigin(인증 X), 시큐리티 필터에 등록 인증(O)
 				.addFilter(corsConfig.corsFilter())
 				.csrf().disable()
+
+				// 세션을 사용하지 않겠다. Stateless 서버
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
+
+				// formlogin 안 쓴다.
 				.formLogin().disable()
+				// 기존에 있는 걸 꺼버리겠다.
+				// 기본적인 http를 안 쓰겠다. 쿠키는 기본적으로 동일 도메인에서 요청을 하면 쿠키가 안 날라간다.
+				// 쿠키는 http only로 설정이 되어서 클라이언트 코드(자바스크립트)에서 접근이 불가능하다.
+				// http only를 풀어주면 보안이 떨어진다.
+				// 세션은 서버 확장성이 떨어진다.
+				// header에 Authorization: "Bearer: fdfdasfwfw" 이렇게 넣는 것이 jwt
+				// http basic은 header에 Authorization: ID, PW 형식으로 사용한다.
+				// 하지만 이걸 사용하면 암호화를 할 수 없어 보안이 떨어진다.
+				// https 서버를 쓰면 암호화가 가능하다.
+				// 그래서 httpBasic을 disable하는 것이다.
+				// 토큰에는 유효시간이 존재한다.
 				.httpBasic().disable()
 				
 				.addFilter(new JwtAuthenticationFilter(authenticationManager()))
 				.addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
 				.authorizeRequests()
+
+				// access는 권한 관련해서 필요하다. mvcMatchers도 있다.
 				.antMatchers("/api/v1/user/**")
 				.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+
 				.antMatchers("/api/v1/manager/**")
 					.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+
 				.antMatchers("/api/v1/admin/**")
 					.access("hasRole('ROLE_ADMIN')")
+
 				.anyRequest().permitAll();
 	}
 }
